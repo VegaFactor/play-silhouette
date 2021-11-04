c87a1c2852f99e80af9878d5130fce151bdf05de
Henry D. Nissenbaum <henry.nissenbaum@vegafactor.com>
2021-11-04 08:26:20 -0400

    version 7.0.4

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -61,7 +61,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "com.vegafactor",
-    version := "7.0.3",
+    version := "7.0.4",
     githubOwner       := "VegaFactor",
     githubRepository  := "play-silhouette",
     githubTokenSource := TokenSource.Environment("GITHUB_TOKEN") || TokenSource.GitConfig("github.token"),
--- silhouette-password-bcrypt/build.sbt
+++ silhouette-password-bcrypt/build.sbt
@@ -1,6 +1,6 @@
 import Dependencies._
 
-version := "7.0.3"
+version := "7.0.4"
 
 libraryDependencies ++= Seq(
   Library.jbcrypt,
